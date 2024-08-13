43da9ca56a63a39006e9f0ff590a2d351a563d47
artem__bakhamatov <ip.bakhmatov@mail.ru>
2024-08-13 22:35:56 +0300

    sprint_1

---
+++ src/pages/chatList/index.ts
@@ -0,0 +1,2 @@
+import './chatList.scss'
+export { default as ChatListPage } from './chatList.hbs?raw';
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { NavigatePage } from './navigation';
+export { LoginPage } from './login';
+export { RegistrationPage } from './registration';
+export { ProfilePage } from './profile';
+export { Error404Page } from './404';
+export { Error500Page } from './500';
+export { ChatListPage } from './chatList';
